e721ef96584e1bb5ab43902fd0b37bd6c0cc3de1
Maceo <basse.maceo@gmail.com>
2023-04-25 10:14:30 +0200

    create pole dev web components

---
+++ src/app/pole-dev-web/pole-dev-web.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pole-dev-web',
+  templateUrl: './pole-dev-web.component.html',
+  styleUrls: ['./pole-dev-web.component.scss']
+})
+export class PoleDevWebComponent {
+
+}
