20cbeea67416ec2f8eaf74d0369e308aaca9646d
Philippe VILARINHO <59869747+tifiloutube@users.noreply.github.com>
2023-04-25 09:00:18 +0200

    [FEATURE] - Mise en place du dashboard en static

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  ngOnInit(): void {
+
+    const modeSwitch = document.querySelector('.mode-switch') as HTMLElement;
+
+    modeSwitch.addEventListener('click', () => {
+      console.log("Hello world")
+      document.documentElement.classList.toggle('dark');
+      modeSwitch.classList.toggle('active');
+    });
+
+    const listView = document.querySelector('.list-view') as HTMLElement;
+    const gridView = document.querySelector('.grid-view') as HTMLElement;
+    const projectsList = document.querySelector('.project-boxes') as HTMLElement;
+
+    listView.addEventListener('click', () => {
+      gridView.classList.remove('active');
+      listView.classList.add('active');
+      projectsList.classList.remove('jsGridView');
+      projectsList.classList.add('jsListView');
+    });
+
+    gridView.addEventListener('click', () => {
+      gridView.classList.add('active');
+      listView.classList.remove('active');
+      projectsList.classList.remove('jsListView');
+      projectsList.classList.add('jsGridView');
+    });
+
+    document.querySelector('.messages-btn')?.addEventListener('click', () => {
+      document.querySelector('.messages-section')?.classList.add('show');
+    });
+
+    document.querySelector('.messages-close')?.addEventListener('click', () => {
+      document.querySelector('.messages-section')?.classList.remove('show');
+    });
+  }
 
 }
