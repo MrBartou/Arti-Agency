9c62f55a7ab64abfa3c1bc683bda54fe4290b470
MrBartou <53816747+MrBartou@users.noreply.github.com>
2023-05-12 01:00:39 +0200

    [ADD] - connection with contact form

--- src/app/site/contact/contact.component.ts
+++ src/app/site/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommandesService } from '../../services/commandes.service';
 
 @Component({
   selector: 'app-contact',
@@ -6,65 +7,76 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
-  toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]') as HTMLInputElement;
-  name = document.getElementById('name') as HTMLInputElement;
-  email = document.getElementById('email') as HTMLInputElement;
-  message = document.getElementById('message') as HTMLInputElement;
-  contactForm = document.getElementById('contact-form') as HTMLFormElement;
-  errorElement = document.getElementById('error') as HTMLElement;
-  successMsg = document.getElementById('success-msg') as HTMLElement;
-  submitBtn = document.getElementById('submit') as HTMLButtonElement;
+  name: string;
+  email: string;
+  message: string;
+  error: string;
+  successMsg: string;
+
+  constructor(private commandesService: CommandesService) {
+    this.name = '';
+    this.email = '';
+    this.message = '';
+    this.error = '';
+    this.successMsg = '';
+  }
 
   validate(e: Event) {
     e.preventDefault();
 
-    if (this.name.value.length < 3) {
-      if (this.errorElement) {
-        this.errorElement.innerHTML = 'Votre nom doit comporter au moins 3 caractères.';
-      }
+    if (this.name.length < 3) {
+      this.error = 'Votre nom doit comporter au moins 3 caractères.';
       return false;
     }
 
-    if (!(this.email.value.includes('.') && this.email.value.includes('@'))) {
-      if (this.errorElement) {
-        this.errorElement.innerHTML = 'Veuillez entrer une adresse email valide.';
-      }
+    if (!(this.email.includes('.') && this.email.includes('@'))) {
+      this.error = 'Veuillez entrer une adresse email valide.';
       return false;
     }
 
-    if (!this.emailIsValid(this.email.value)) {
-      if (this.errorElement) {
-        this.errorElement.innerHTML = 'Veuillez entrer une adresse email valide.';
-      }
+    if (!this.emailIsValid(this.email)) {
+      this.error = 'Veuillez entrer une adresse email valide.';
       return false;
     }
 
-    if (this.message.value.length < 15) {
-      if (this.errorElement) {
-        this.errorElement.innerHTML = 'Veuillez écrire un message plus long.';
-      }
+    if (this.message.length < 15) {
+      this.error = 'Veuillez écrire un message plus long.';
       return false;
     }
 
-    if (this.errorElement) {
-      this.errorElement.innerHTML = '';
-    }
+    this.error = '';
 
-    if (this.successMsg) {
-      this.successMsg.innerHTML = 'Merci! Je vous répondrai dès que possible.';
-    }
+    this.successMsg = 'Merci! Je vous répondrai dès que possible.';
 
-    setTimeout(() => {
-      if (this.successMsg) {
-        this.successMsg.innerHTML = '';
-        this.contactForm.reset();
-      }
-    }, 6000);
+    const commande = {
+      name: this.name,
+      avatar: 'https://picsum.photos/200/300',
+      message: this.message,
+      date: new Date().toISOString().slice(0, 10),
+    };
+
+    this.commandesService.addCommande(commande)
+      .then(() => {
+        setTimeout(() => {
+          this.successMsg = '';
+          this.resetForm();
+        }, 6000);
+      })
+      .catch((error) => {
+        console.error('Erreur lors de l\'ajout de la commande:', error);
+        this.error = 'Une erreur s\'est produite lors de l\'envoi du formulaire. Veuillez réessayer.';
+      });
 
     return true;
   }
 
   emailIsValid(email: string) {
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  }
+
+  resetForm() {
+    this.name = '';
+    this.email = '';
+    this.message = '';
   }
 }
