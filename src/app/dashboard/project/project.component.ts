c78f88f7b9cbcf930c8f2bfb595b7dc95f76bcaf
Philippe VILARINHO <59869747+tifiloutube@users.noreply.github.com>
2023-05-12 23:59:54 +0200

    [FEATURE] - Drag&Drop fonctionnel

--- src/app/dashboard/project/project.component.ts
+++ src/app/dashboard/project/project.component.ts
@@ -1,11 +1,146 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.scss']
 })
-export class ProjectComponent {
+export class ProjectComponent implements AfterViewInit {
+  private dragSrcEl: HTMLElement | null = null;
 
+  constructor(private el: ElementRef) { }
+
+  ngAfterViewInit() {
+    this.addDragDropFunctionality();
+    this.addDropFunctionalityToColumns();
+    this.updateProgressBars();
+  }
+
+  private addDragDropFunctionality() {
+    const items = this.el.nativeElement.querySelectorAll('.task');
+    items.forEach((item: HTMLElement) => {
+      item.addEventListener('dragstart', this.handleDragStart.bind(this), false);
+      item.addEventListener('dragenter', this.handleDragEnter.bind(this), false);
+      item.addEventListener('dragover', this.handleDragOver.bind(this), false);
+      item.addEventListener('dragleave', this.handleDragLeave.bind(this), false);
+      item.addEventListener('drop', this.handleDrop.bind(this), false);
+      item.addEventListener('dragend', this.handleDragEnd.bind(this), false);
+    });
+  }
+
+  private addDropFunctionalityToColumns() {
+    const columns = this.el.nativeElement.querySelectorAll('.project-column');
+    columns.forEach((column: HTMLElement) => {
+      column.addEventListener('dragover', this.handleColumnDragOver.bind(this), false);
+      column.addEventListener('drop', this.handleColumnDrop.bind(this), false);
+    });
+  }
+
+  private handleDragStart(e: DragEvent) {
+    if (!e.target) return;
+    const target = e.target as HTMLElement;
+    target.style.opacity = '0.1';
+    target.style.border = '3px dashed #c4cad3';
+
+    this.dragSrcEl = target;
+
+    e.dataTransfer!.effectAllowed = 'move';
+    e.dataTransfer!.setData('text/html', this.dragSrcEl.outerHTML);
+  }
+
+  private handleDragOver(e: DragEvent) {
+    if (e.preventDefault) {
+      e.preventDefault();
+    }
+    e.dataTransfer!.dropEffect = 'move';
+
+    return false;
+  }
+
+  private handleDragEnter(e: Event) {
+    if (!e.target) return;
+    const target = e.target as HTMLElement;
+    target.classList.add('task-hover');
+  }
+
+  private handleDragLeave(e: Event) {
+    if (!e.target) return;
+    const target = e.target as HTMLElement;
+    target.classList.remove('task-hover');
+  }
+
+  private handleDrop(e: DragEvent) {
+    if (!e.target) return;
+    const target = e.target as HTMLElement;
+
+    if (e.stopPropagation) {
+      e.stopPropagation(); // stops the browser from redirecting.
+    }
+
+    if (this.dragSrcEl && this.dragSrcEl !== target) {
+      this.dragSrcEl.innerHTML = target.innerHTML;
+      target.innerHTML = e.dataTransfer!.getData('text/html');
+    }
+
+    this.updateProgressBars();
+
+    return false;
+  }
+
+  private handleDragEnd(e: Event) {
+    if (!e.target) return;
+    const target = e.target as HTMLElement;
+    target.style.opacity = '1';
+    target.style.border = '0';
+
+    const items = this.el.nativeElement.querySelectorAll('.task');
+    items.forEach((item: HTMLElement) => {
+      item.classList.remove('task-hover');
+    });
+  }
+
+  private handleColumnDragOver(e: DragEvent) {
+    if (e.preventDefault) {
+      e.preventDefault();
+    }
+    e.dataTransfer!.dropEffect = 'move';
+
+    return false;
+  }
+
+  private handleColumnDrop(e: DragEvent) {
+    if (!e.target) return;
+    const target = e.target as HTMLElement;
+
+    if (e.stopPropagation) {
+      e.stopPropagation(); // stops the browser from redirecting.
+    }
+
+    if (this.dragSrcEl) {
+      target.appendChild(this.dragSrcEl);
+      this.dragSrcEl.style.opacity = '1';
+      this.dragSrcEl.style.border = '0';
+    }
+
+    this.updateProgressBars();
+
+    return false;
+  }
+
+  private updateProgressBars() {
+    const categories = ['Copywriting', 'Illustration', 'UI Design'];
+    const doneColumn = this.el.nativeElement.querySelector('.project-column[title="done"]');
+
+    categories.forEach(category => {
+      const totalTasks = this.el.nativeElement.querySelectorAll(`.task[data-category="${category}"]`).length;
+      const doneTasks = doneColumn.querySelectorAll(`.task[data-category="${category}"]`).length;
+
+      const progressBar = this.el.nativeElement.querySelector(`.tag-progress[data-category="${category}"]`);
+      if (progressBar) {
+        progressBar.setAttribute('max', totalTasks.toString());
+        progressBar.setAttribute('value', doneTasks.toString());
+      }
+    });
+  }
 }
 
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -9,8 +9,109 @@ export class TaskService {
   private dbName = 'taskDB';
   private objectStoreName = 'tasks';
   private tasksSubject: Subject<Task[]> = new Subject<Task[]>();
+  private hasAddedFakeTasks = false;
 
-  constructor() { }
+  constructor() {
+    this.addFakeTask();
+  }
+
+  private async addFakeTask(): Promise<void> {
+    const existingTasks = await this.getTasksFromDatabase();
+    if (existingTasks.length > 0 || this.hasAddedFakeTasks) {
+      return;
+    }
+
+    const fakeTasks: Task[] = [
+      {
+        projectName: 'Projet A',
+        memberAvatar: 'avatar1.jpg',
+        endDate: 'Nov 24',
+        taskName: 'Tâche 1',
+        taskStatus: 'todo',
+        department: 'Développement'
+      },
+      {
+        projectName: 'Projet A',
+        memberAvatar: 'avatar2.jpg',
+        endDate: 'Nov 24',
+        taskName: 'Tâche 2',
+        taskStatus: 'progress',
+        department: 'Marketing'
+      },
+      {
+        projectName: 'Projet A',
+        memberAvatar: 'avatar3.jpg',
+        endDate: 'Nov 24',
+        taskName: 'Tâche 3',
+        taskStatus: 'review',
+        department: 'Création'
+      },
+      {
+        projectName: 'Projet A',
+        memberAvatar: 'avatar4.jpg',
+        endDate: 'Nov 24',
+        taskName: 'Tâche 4',
+        taskStatus: 'done',
+        department: 'Développement'
+      },
+      {
+        projectName: 'Projet A',
+        memberAvatar: 'avatar5.jpg',
+        endDate: 'Nov 24',
+        taskName: 'Tâche 5',
+        taskStatus: 'progress',
+        department: 'Marketing'
+      },
+      {
+        projectName: 'Projet B',
+        memberAvatar: 'avatar6.jpg',
+        endDate: 'Nov 24',
+        taskName: 'Tâche 1',
+        taskStatus: 'done',
+        department: 'Développement'
+      },
+      {
+        projectName: 'Projet B',
+        memberAvatar: 'avatar7.jpg',
+        endDate: 'Nov 24',
+        taskName: 'Tâche 2',
+        taskStatus: 'progress',
+        department: 'Marketing'
+      },
+      {
+        projectName: 'Projet B',
+        memberAvatar: 'avatar8.jpg',
+        endDate: 'Nov 24',
+        taskName: 'Tâche 3',
+        taskStatus: 'review',
+        department: 'Création'
+      },
+      {
+        projectName: 'Projet B',
+        memberAvatar: 'avatar9.jpg',
+        endDate: 'Nov 24',
+        taskName: 'Tâche 4',
+        taskStatus: 'done',
+        department: 'Développement'
+      },
+      {
+        projectName: 'Projet B',
+        memberAvatar: 'avatar10.jpg',
+        endDate: 'Nov 24',
+        taskName: 'Tâche 5',
+        taskStatus: 'todo',
+        department: 'Marketing'
+      }
+    ];
+
+    fakeTasks.forEach((Tasks) => {
+      this.addTask(Tasks)
+        .then(() => console.log('Tasks ajouté avec succès:', Tasks))
+        .catch((error) => console.error('Erreur lors de l\'ajout des tasks:', error));
+    });
+
+    this.hasAddedFakeTasks = true;
+  }
 
   async addTask(task: Task): Promise<void> {
     const db = await this.openDatabase();
@@ -23,6 +124,71 @@ export class TaskService {
   getTasks(): Observable<Task[]> {
     this.fetchTasks();
     return this.tasksSubject.asObservable();
+  }
+
+  getTaskByDepartment(department: string, projectName?: string): Observable<number> {
+    return new Observable<number>((observer) => {
+      this.getTasks()
+        .subscribe((tasks: Task[]) => {
+          let filteredTasks = tasks.filter((task) => task.department === department);
+
+          if (projectName) {
+            filteredTasks = filteredTasks.filter((task) => task.projectName === projectName);
+          }
+
+          const completedTasks = filteredTasks.filter((task) => task.taskStatus === 'Terminée');
+          const completionPercentage = (completedTasks.length / filteredTasks.length) * 100;
+          observer.next(completionPercentage);
+        });
+    });
+  }
+
+  getTasksByStatus(status: string, projectName?: string): Observable<Task[]> {
+    return new Observable<Task[]>((observer) => {
+      this.getTasks()
+        .subscribe((tasks: Task[]) => {
+          let filteredTasks = tasks.filter((task) => task.taskStatus === status);
+
+          if (projectName) {
+            filteredTasks = filteredTasks.filter((task) => task.projectName === projectName);
+          }
+
+          observer.next(filteredTasks);
+        });
+    });
+  }
+
+  getTasksByProject(projectName: string): Observable<Task[]> {
+    return new Observable<Task[]>((observer) => {
+      this.getTasks()
+        .subscribe((tasks: Task[]) => {
+          const filteredTasks = tasks.filter((task) => task.projectName === projectName);
+          observer.next(filteredTasks);
+        });
+    });
+  }
+
+  updateTask(task: Task): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      this.openDatabase()
+        .then((db: IDBDatabase) => {
+          const transaction = db.transaction(this.objectStoreName, 'readwrite');
+          const objectStore = transaction.objectStore(this.objectStoreName);
+          const request = objectStore.put(task);
+
+          request.onsuccess = () => {
+            this.fetchTasks();
+            resolve();
+          };
+
+          request.onerror = (event: any) => {
+            reject(request.error);
+          };
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
   }
 
   async deleteTask(id: number): Promise<void> {
